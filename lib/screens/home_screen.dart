402e1f47c87c91cb2515af5e9d2bb75fad627557
Djali Suandre <djalisuandre_2226240011@mhs.mdp.ac.id>
2025-01-04 08:56:37 +0700

    Add Home Screen w/o data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,89 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // Make A Scaffold first
+    return Scaffold(
+      // TODO: Add appBar
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Row(
+          children: [
+            CircleAvatar(
+              backgroundColor: Colors.amber,
+              child: Icon(Icons.person, color: Colors.white),
+            ),
+            SizedBox(width: 10),
+            Text('Hi, Username'),
+          ],
+        ),
+        // TODO: Add Actions button
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+        ],
+        // TODO: Making the search bar more rich with icons and fixSize
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Container(
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search here',
+                  border: InputBorder.none,
+                  prefixIcon: Icon(Icons.menu),
+                  suffixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      // TODO: Add Body after header(AppBar)
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GridView.builder(
+          itemCount: 12,
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, crossAxisSpacing: 8, mainAxisSpacing: 8),
+          itemBuilder: (context, index) {
+            return Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 4,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.image, size: 40),
+                  SizedBox(height: 8),
+                  Text('Title'),
+                  SizedBox(height: 4),
+                  Text(
+                    'Updated today',
+                    style: TextStyle(fontSize: 12, color: Colors.grey),
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
