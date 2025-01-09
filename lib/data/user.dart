6d04174fd6ed9c29a9b26c90c772a49cef89a225
Angel <160828167+miawpuspita@users.noreply.github.com>
2025-01-09 14:08:25 +0700

    push

---
+++ lib/data/user.dart
@@ -0,0 +1,7 @@
+import 'package:tugas_akhirpab1/models/user.dart';
+
+// Daftar pengguna yang terdaftar
+var userList = [
+  User(name: 'puspita', email: 'puspita@gmail.com', password: '12345'),
+  User(name: 'sari', email: 'sari@gmail.com', password: '67890'),
+];
