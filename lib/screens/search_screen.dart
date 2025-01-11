0cf74e0870f8eca3f506ebf450c5dbc92f9e8fff
Aprol <dyonfilipo@gmail.com>
2025-01-11 21:43:17 +0700

    edit profile and search

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -5,6 +5,35 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.orange[200],
+        elevation: 0,
+        title: TextField(
+          decoration: InputDecoration(
+            hintText: 'Hinted search text',
+            prefixIcon: Icon(Icons.search),
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemCount: 2,
+        itemBuilder: (context, index) {
+          return Card(
+            margin: EdgeInsets.symmetric(vertical: 8),
+            child: ListTile(
+              leading: Icon(Icons.image, size: 40, color: Colors.grey),
+              title: Text('title'),
+              onTap: () {
+                // Aksi saat item di klik
+                print('Clicked on ');
+              },
+            ),
+          );
+        },
+      ),
+    );
   }
 }
