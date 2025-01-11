44bf9df2a9245896d96140c9cf8ae46c0aa0b860
Aprol <dyonfilipo@gmail.com>
2025-01-11 22:35:37 +0700

    Update search_screen.dart

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,7 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:tugas_akhirpab1/models/home.dart';
+import 'package:tugas_akhirpab1/data/home_data.dart';
+import 'package:tugas_akhirpab1/screens/detail_screen.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  TextEditingController searchController = TextEditingController();
+  List<kerajinan> filteredList = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredList = kerajinanList; // Menampilkan semua data secara default
+  }
+
+  void filterData(String keyword) {
+    setState(() {
+      if (keyword.isEmpty) {
+        filteredList = kerajinanList; // Jika kosong, tampilkan semua data
+      } else {
+        filteredList = kerajinanList
+            .where((item) =>
+                item.nama.toLowerCase().contains(keyword.toLowerCase()))
+            .toList(); // Filter berdasarkan kata kunci
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +40,8 @@ class SearchScreen extends StatelessWidget {
         backgroundColor: Colors.orange[200],
         elevation: 0,
         title: TextField(
+          controller: searchController,
+          onChanged: filterData,
           decoration: InputDecoration(
             hintText: 'Hinted search text',
             prefixIcon: Icon(Icons.search),
@@ -19,16 +51,24 @@ class SearchScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         padding: EdgeInsets.all(16),
-        itemCount: 2,
+        itemCount: filteredList.length,
         itemBuilder: (context, index) {
+          var item = filteredList[index];
+          kerajinan varkerajinan = filteredList[index];
           return Card(
             margin: EdgeInsets.symmetric(vertical: 8),
             child: ListTile(
-              leading: Icon(Icons.image, size: 40, color: Colors.grey),
-              title: Text('title'),
+              leading: Image.network(item.gambar, height: 40, width: 40),
+              title: Text(item.nama),
               onTap: () {
                 // Aksi saat item di klik
-                print('Clicked on ');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        DetailScreen(varkerajinan: varkerajinan),
+                  ),
+                );
               },
             ),
           );
